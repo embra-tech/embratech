0ea49f43d2b295fc710621ea41c27971ea2d9fe8
Bilaal <mr.rahman.professional@gmail.com>
2026-09-15 02:08:07 +0530

    Integrate @vercel/speed-insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import WhatsAppButton from '../components/WhatsAppButton';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const figtree = Figtree({
   subsets: ['latin'],
@@ -57,9 +58,11 @@ export default function RootLayout({ children }) {
             <Footer />
             <WhatsAppButton />
             <Analytics />
+            <SpeedInsights />
           </ScrollManager>
         </ThemeProvider>
       </body>
     </html>
   );
 }
+
